1bceba5282ff8d51c5845d6096ec2ca54ea4da2f
K4zzz <136621410+Kazuhaaa01@users.noreply.github.com>
2026-05-05 18:10:49 +0200

    Prueba

---
+++ M3/src/civilizations/abstractas.java
@@ -0,0 +1,7 @@
+package civilizations;
+
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("¡Hola! Esto es una prueba de salida en consola.");
+    }
+}
